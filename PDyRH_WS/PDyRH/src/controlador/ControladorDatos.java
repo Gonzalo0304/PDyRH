0de957a256b2c64aab715576938d2b26df34f3f5
Haizea <haizea.franco@gmail.com>
2022-04-06 09:58:45 +0200

    Conexión con BD: Sin terminar

--- PDyRH_WS/PDyRH/src/controlador/ControladorDatos.java
+++ PDyRH_WS/PDyRH/src/controlador/ControladorDatos.java
@@ -1,7 +1,80 @@
 package controlador;
 
+import java.util.Map;
+
+import modelo.clases.Agente;
+import modelo.clases.Banda;
+import modelo.clases.Caso;
+import modelo.clases.Criminal;
+import modelo.clases.Desaparecida;
 import modelo.clases.Persona;
+import modelo.clases.RestoHumano;
 
 public interface ControladorDatos {
+	// <--- Persona --->
+	public void altaPersona(Persona per);
+	public void modificarPersona(Persona per);
+	public void eliminarPersona(Persona per);
+	public Persona buscarPersona(String dni);
+	public Map<String, Persona> listarPersonas();
+	
+	// <--- Agente --->
+	public void altaAgente(Agente age);
+	public void modificarAgente(Agente age);
+	public void eliminarAgente(Agente age);
+	
+	// <--- Criminal --->
+	public void altaCriminal(Criminal crim);
+	public void modificarCriminal(Criminal crim);
+	public void eliminarCriminal(Criminal crim);
+	
+	// <--- Desaparecida --->
+	public void altaDesaparecida(Desaparecida des);
+	public void modificarDesaparecida(Desaparecida des);
+	public void eliminarDesaparecida(Desaparecida des);
+	
+	// <--- Resto Humano --->
+	public void altaRH(RestoHumano rh);
+	public void modificarRH(RestoHumano rh);
+	public void eliminarRH(RestoHumano rh);
+	public RestoHumano buscarRH(String codResto);
+	public Map<String, RestoHumano> listarRestos();
+	
+	// <--- Banda ---->
+	public void altaBanda(Banda banda);
+	public void modificarBanda(Banda banda);
+	public void eliminarBanda(Banda banda);
+	public Banda buscarBanda(String codBanda);
+	public Map<String, Banda> listarBandas();
+	
+	// <--- Caso --->
+	public void altaCaso(Caso caso);
+	public void modificarCaso(Caso caso);
+	public void eliminarCaso(Caso caso);
+	public Caso buscarCaso(String codCaso);
+	public Map<String, Caso> listarCasos();
 
-}
+	// <--- Involucrado --->
+	public void agregarInvolucrado(String codBanda, Caso caso);
+	public Map<String,Banda> listarInvolucrados(String codCaso);
+	
+	// <--- Participa --->
+	public void agregarParticipante(String dni, Caso caso, String implicacion);
+	public Map<String,Persona> listarParticipantes(String codCaso);
+	
+	// <--- Identifica --->
+	public void agregarIdentificado(RestoHumano resto, Persona per);
+	public Persona buscarIdentificado(String codResto);
+	
+	// <--- Conoce --->
+	public void agregarConocido(Persona per, String dni2, String relacion);
+	public Map<String,Persona> listarConocidos(String dni1);
+	
+	// <--- Pertenece --->
+	public void agregarPerteneciente(Banda banda, String dni, String rol);
+	public Map<String,Persona> listarMiembros(String codBanda);
+	
+	// <--- Trabaja --->
+	public void agregarRelacion(Banda banda, String codBanda2);
+	public Map<String,Banda> listarBandas(String codBanda);
+ }
--- PDyRH_WS/PDyRH/src/modelo/ControladorBDImplementacion.java
+++ PDyRH_WS/PDyRH/src/modelo/ControladorBDImplementacion.java
@@ -1,14 +1,40 @@
 package modelo;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.TreeMap;
 
 import controlador.ControladorDatos;
+import modelo.clases.Agente;
+import modelo.clases.Banda;
+import modelo.clases.Caso;
+import modelo.clases.Criminal;
+import modelo.clases.Desaparecida;
+import modelo.clases.Persona;
+import modelo.clases.RestoHumano;
 
 public class ControladorBDImplementacion implements ControladorDatos {
+	// <--- Sentencias --->
+	final String INSERTper = "INSERT INTO persona(dni,nombre,apellido,telf1,localidad,fechaNac,fechaFal,telf2) VALUES(?,?,?,?,?,?,?,?)";
+	final String UPDATEper = "UPDATE persona SET nombre = ?,apellido = ?,telf1 = ?,localidad = ?,fechaNac = ?,fechaFal = ?,telf2 = ? WHERE dni = ?";
+	final String DELETEper = "DELETE persona WHERE dni = ?";
+	final String SELECTper = "SELECT * FROM persona WHERE dni = ?";
+	final String SELECTpers = "SELECT * FROM persona";
+	
+	final String INSERTconoce = "INSERT INTO conoce(dniP1,dniP2,relacion) VALUES(?,?,?)";
+	final String INSERTinvolucrado = "INSERT INTO involucrado(codCaso,codBanda) VALUES(?,?)";
+	final String INSERTparticipa = "INSERT INTO participa(codCaso,dni,implicación) VALUES(?,?,?)";
+	final String INSERTidentifica = "INSERT INTO identifica(dni,codResto) VALUES(?,?)";
+	final String INSERTpertenece = "INSERT INTO pertenece(codBanda,dni,rol) VALUES(?,?,?)";
+	final String INSERTtrabaja = "INSERT INTO trabaja(codBanda1,codBanda2) VALUES(?,?)";
+	
+	// <--- Conexión --->
 	private PreparedStatement stmnt;
 	private Connection con;
 	
@@ -44,6 +70,452 @@ public class ControladorBDImplementacion implements ControladorDatos {
 				e.printStackTrace();
 			}
 		}
+	}
+	
+	// <--- Persona --->
+	@Override
+	public void altaPersona(Persona per) {
+		this.openConnection();
+		
+		try {
+			stmnt = con.prepareStatement(INSERTper);
+			
+			stmnt.setString(1, per.getDni());
+			stmnt.setString(2, per.getNombre());
+			stmnt.setString(3, per.getApellido());
+			stmnt.setInt(4, per.getTelefonos()[0]);
+			stmnt.setString(5, per.getLocalidad());
+			stmnt.setDate(6, Date.valueOf(per.getFechaNac()));
+			stmnt.setDate(7, Date.valueOf(per.getFechaFal()));
+			stmnt.setInt(8, per.getTelefonos()[1]);
+			
+			stmnt.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		this.closeConnection();
+	}
+
+	@Override
+	public void modificarPersona(Persona per) {
+		this.openConnection();
+		
+		try {
+			stmnt = con.prepareStatement(UPDATEper);
+			
+			stmnt.setString(1, per.getNombre());
+			stmnt.setString(2, per.getApellido());
+			stmnt.setInt(3, per.getTelefonos()[0]);
+			stmnt.setString(4, per.getLocalidad());
+			stmnt.setDate(5, Date.valueOf(per.getFechaNac()));
+			stmnt.setDate(6, Date.valueOf(per.getFechaFal()));
+			stmnt.setInt(7, per.getTelefonos()[1]);
+			stmnt.setString(8, per.getDni());
+			
+			stmnt.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		this.closeConnection();	
+	}
+
+	@Override
+	public void eliminarPersona(Persona per) {
+		this.openConnection();
+		
+		try {
+			stmnt = con.prepareStatement(DELETEper);
+			
+			stmnt.setString(1, per.getDni());
+			
+			stmnt.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		this.closeConnection();	
+	}
+
+	@Override
+	public Persona buscarPersona(String dni) {
+		ResultSet rs = null;
+		Persona per = null;
+		
+		this.openConnection();
+		
+		try {
+			stmnt = con.prepareStatement(SELECTper);
+			stmnt.setString(1, dni);
+			
+			rs = stmnt.executeQuery();
+			
+			if (rs.next()) {
+				int[] telfs = {rs.getInt("telf1"),rs.getInt("telf2")};
+				per = new Persona();
+				
+				per.setDni(dni);
+				per.setNombre(rs.getString("nombre"));
+				per.setApellido(rs.getString("apellido"));
+				per.setTelefonos(telfs);
+				per.setFechaNac(rs.getDate("fechaNac").toLocalDate());
+				per.setFechaFal(rs.getDate("fechaFal").toLocalDate());
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		this.closeConnection();
+		return per;
+	}
+
+	@Override
+	public Map<String, Persona> listarPersonas() {
+		ResultSet rs = null;
+		Persona per;
+		Map<String,Persona> personas = new TreeMap<>();
+		
+		this.openConnection();
+		
+		try {
+			stmnt = con.prepareStatement(SELECTpers);
+			
+			rs = stmnt.executeQuery();
+			
+			while (rs.next()) {
+				int[] telfs = {rs.getInt("telf1"),rs.getInt("telf2")};
+				per = new Persona();
+				
+				per.setDni(rs.getString("dni"));
+				per.setNombre(rs.getString("nombre"));
+				per.setApellido(rs.getString("apellido"));
+				per.setTelefonos(telfs);
+				per.setFechaNac(rs.getDate("fechaNac").toLocalDate());
+				per.setFechaFal(rs.getDate("fechaFal").toLocalDate());
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		this.closeConnection();
+		return personas;
+	}
+	
+	// <--- Agente --->
+	@Override
+	public void altaAgente(Agente age) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void modificarAgente(Agente age) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void eliminarAgente(Agente age) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	// <--- Criminal --->
+	@Override
+	public void altaCriminal(Criminal crim) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void modificarCriminal(Criminal crim) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void eliminarCriminal(Criminal crim) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	// <--- Desaparecida --->
+	@Override
+	public void altaDesaparecida(Desaparecida des) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void modificarDesaparecida(Desaparecida des) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void eliminarDesaparecida(Desaparecida des) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	// <--- Resto Humano --->
+	@Override
+	public void altaRH(RestoHumano rh) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void modificarRH(RestoHumano rh) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void eliminarRH(RestoHumano rh) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public RestoHumano buscarRH(String codResto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, RestoHumano> listarRestos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	// <--- Banda --->
+	@Override
+	public void altaBanda(Banda banda) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void modificarBanda(Banda banda) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void eliminarBanda(Banda banda) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Banda buscarBanda(String codBanda) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, Banda> listarBandas() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	// <--- Caso --->
+	@Override
+	public void altaCaso(Caso caso) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void modificarCaso(Caso caso) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void eliminarCaso(Caso caso) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Caso buscarCaso(String codCaso) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public Map<String, Caso> listarCasos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	// <--- Involucrado --->
+	@Override
+	public void agregarInvolucrado(String codBanda, Caso caso) {
+		this.openConnection();
+		
+		try {
+			stmnt = con.prepareStatement(INSERTinvolucrado);
+			
+			stmnt.setString(1, caso.getCodCaso());
+			stmnt.setString(2, codBanda);
+			
+			stmnt.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		this.closeConnection();
+	}
+
+	@Override
+	public Map<String, Banda> listarInvolucrados(String codCaso) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	// <--- Participa --->
+	@Override
+	public void agregarParticipante(String dni, Caso caso, String implicacion) {
+		this.openConnection();
+		
+		try {
+			stmnt = con.prepareStatement(INSERTparticipa);
+			
+			stmnt.setString(1, caso.getCodCaso());
+			stmnt.setString(2, dni);
+			stmnt.setString(3, implicacion);
+			
+			stmnt.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		this.closeConnection();
+	}
+
+	@Override
+	public Map<String, Persona> listarParticipantes(String codCaso) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	// <--- Identifica --->
+	@Override
+	public void agregarIdentificado(RestoHumano resto, Persona per) {
+		this.openConnection();
+		
+		try {
+			stmnt = con.prepareStatement(INSERTparticipa);
+			
+			stmnt.setString(1, resto.getCodResto());
+			stmnt.setString(2, per.getDni());
+			
+			stmnt.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		this.closeConnection();
+	}
+
+	@Override
+	public Persona buscarIdentificado(String codResto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	// <--- Conoce --->
+	@Override
+	public void agregarConocido(Persona per, String dni2, String relacion) {
+		this.openConnection();
+		
+		try {
+			stmnt = con.prepareStatement(INSERTconoce);
+			
+			stmnt.setString(1, per.getDni());
+			stmnt.setString(2, dni2);
+			stmnt.setString(3, relacion);
+			
+			stmnt.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		this.closeConnection();
+	}
+
+	@Override
+	public Map<String, Persona> listarConocidos(String dni1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	// <--- Pertenece --->
+	@Override
+	public void agregarPerteneciente(Banda banda, String dni, String rol) {
+		this.openConnection();
+		
+		try {
+			stmnt = con.prepareStatement(INSERTpertenece);
+			
+			stmnt.setString(1, banda.getCodBanda());
+			stmnt.setString(2, dni);
+			stmnt.setString(3, rol);
+			
+			stmnt.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		this.closeConnection();
+	}
+
+	@Override
+	public Map<String, Persona> listarMiembros(String codBanda) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	// <--- Trabaja --->
+	@Override
+	public void agregarRelacion(Banda banda, String codBanda2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Map<String, Banda> listarBandas(String codBanda) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 }
